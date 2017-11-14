ee8de1eb989b774d5f6a68d90c94a91d994a6cfb
Ottavio Campana <ottavio@campana.vi.it>
2017-11-14 09:09:03 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -18,7 +18,7 @@ class Heroku::Command::Vim < Heroku::Command::Run
     <<-CMD
 mkdir vim
 curl https://s3.amazonaws.com/bengoa/vim-static.tar.gz --location --silent | tar xz -C vim
-export PATH=$PATH:/app/vim/bin
+export PATH=$PATH:/app/vim/
 bash
     CMD
   end
